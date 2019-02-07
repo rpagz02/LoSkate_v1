4817e89b986587115203457b520e3d07cce49849
rpagz02 <rpagz02@gmail.com>
2019-02-07 18:55:46 -0500

    2/7/2019 commit 3: Map with location and rudimentary marker dropping working ! Next Step is storing marker information in a database || ability to take a picture when the marker is dropped and store

--- app/src/main/java/com/example/loskate0/Map_Fragment.java
+++ app/src/main/java/com/example/loskate0/Map_Fragment.java
@@ -1,7 +1,10 @@
 package com.example.loskate0;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,14 +13,19 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Map_Fragment extends Fragment implements OnMapReadyCallback {
 
@@ -27,27 +35,31 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
     GoogleMap gMap;
     private boolean mLocationPermissionGranted = false;
     private static final int mLocationCode = 1234;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     /* Variables */
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this.getContext(), "Drop A Spot!", Toast.LENGTH_SHORT).show();
         gMap = googleMap;
-//     LatLng coords = new LatLng(11, 105);
-//     MarkerOptions markerOptions = new MarkerOptions();
-//     markerOptions.position(coords).title("Temp Location");
-//     gMap.addMarker(markerOptions);
-//     gMap.moveCamera(CameraUpdateFactory.newLatLng(coords));
-
-        Toast.makeText(this.getContext(),"Drop A Spot!", Toast.LENGTH_SHORT).show();
+        if (mLocationPermissionGranted) {
+            GetDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                return;
+            gMap.setMyLocationEnabled(true);
+            gMap.getUiSettings().setCompassEnabled(true);
+            gMap.getUiSettings().setMapToolbarEnabled(true);
+        }
     }
-
 
     // Check our Permissions in this method
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GetLocationPermission();
+
+
     }
 
     // Inflate the fragment here
@@ -55,13 +67,23 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        // Handle Button Stuff Here
+        Button markerButton = (Button) rootView.findViewById(R.id.MapMarkerButton);
+        markerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // drop a marker
+                DropMarker();
+            }
+        });
+        // Handle Map Stuff here
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment.getMapAsync(this);
         return rootView;
     }
 
     // Create the supportMapFragment and initialize the map
-    private void InitMap(){
+    private void InitMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment.getMapAsync(this);
     }
@@ -69,29 +91,28 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
     // Check Permissions from the user to access location (Fine & Course)
     private void GetLocationPermission() {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-       if(ContextCompat.checkSelfPermission(this.getContext(),FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-        if(ContextCompat.checkSelfPermission(this.getContext(),COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        }else{
-               ActivityCompat.requestPermissions(this.getActivity(),permissions,mLocationCode);
-           }
-       }else{
-           ActivityCompat.requestPermissions(this.getActivity(),permissions,mLocationCode);
-       }
+        if (ContextCompat.checkSelfPermission(this.getContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this.getActivity(), permissions, mLocationCode);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this.getActivity(), permissions, mLocationCode);
+        }
     }
+
     // Method that checks what to do when permissions are either granted or denied
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         mLocationPermissionGranted = false;
 
-        switch(requestCode){
-            case mLocationCode:
-            {
-                if(grantResults.length > 0 )
-                {
-                    for(int i = 0; i < grantResults.length; i++ ) // Loop through our permissions to see if they are granted
+        switch (requestCode) {
+            case mLocationCode: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) // Loop through our permissions to see if they are granted
                     {
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) // if 1 permission returns false, we return
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) // if 1 permission returns false, we return
                         {
                             mLocationPermissionGranted = false;
                             return;
@@ -101,8 +122,60 @@ public class Map_Fragment extends Fragment implements OnMapReadyCallback {
                     //initialize our map
                     InitMap();
                 }
-               break;
+                break;
             }
         }
     }
+
+    // LOCATION METHODS //
+
+    private void GetDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+        try {
+            if (mLocationPermissionGranted) {
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location currentLocation = (Location) task.getResult();
+
+                            MoveMapCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 15f);
+//                        gMap.addMarker(new MarkerOptions().position(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())).title("Current Spot"));
+
+                        } else {
+                            Toast.makeText(Map_Fragment.this.getContext(), "unable to get location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+        }
+    }
+
+    private void MoveMapCamera(LatLng latLng, float zoom) {
+        Toast.makeText(Map_Fragment.this.getContext(), "Peepin Your Location", Toast.LENGTH_SHORT).show();
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void DropMarker() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getContext());
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        final Task location = mFusedLocationProviderClient.getLastLocation();
+        location.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful())
+                {
+                    Location currentLocation = (Location) task.getResult();
+                    gMap.addMarker(new MarkerOptions().position(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())).title("Current Spot"));
+                } else
+                    Toast.makeText(Map_Fragment.this.getContext(), "unable to get location", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
